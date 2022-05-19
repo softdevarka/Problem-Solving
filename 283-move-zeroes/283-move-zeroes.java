0f7fd33e788c4396252000e4b8c1b9d9b2debb20
Arka Raha <55012941+softdevarka@users.noreply.github.com>
2022-05-20 01:34:22 +0530

    Time: 3 ms (28.69%), Space: 54.9 MB (41.26%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i=0; i<n; i++){
+            if (nums[i] != 0){
+                int temp = nums[count];
+                nums[count] = nums[i];
+                nums[i] = temp;
+            count++;
+        }
+        
+    }
+}
+
+}
