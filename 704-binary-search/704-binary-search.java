a5ea1eda09241a7981e7b238ed65d1f3c27f707b
Arka Raha <55012941+softdevarka@users.noreply.github.com>
2022-06-19 13:23:36 +0530

    Time: 0 ms (100.00%), Space: 54.4 MB (39.08%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int low = 0;
+//         int high = nums.length - 1;
+//         return bSearchRecursive(nums, low, high, target);
+//         }
+//     public int bSearchRecursive(int nums[], int low, int high, int target){
+//         if (low>high)
+//             return -1;
+//         int mid = (low+high)/2;
+//         if (nums[mid] == target)
+//             return mid;
+//         else if (nums[mid]>target)
+//             return bSearchRecursive(nums, low, mid-1, target);
+//         else
+//             return bSearchRecursive(nums, mid+1, high, target);
+//     }
+// }
+
 class Solution {
-    public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        return bSearchRecursive(nums, low, high, target);
+    public int search (int nums[], int target){
+        int n = nums.length;
+        int low = 0, high = n-1;
+        while (low<=high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+            
         }
-    public int bSearchRecursive(int nums[], int low, int high, int target){
-        if (low>high)
-            return -1;
-        int mid = (low+high)/2;
-        if (nums[mid] == target)
-            return mid;
-        else if (nums[mid]>target)
-            return bSearchRecursive(nums, low, mid-1, target);
-        else
-            return bSearchRecursive(nums, mid+1, high, target);
+        return -1;
     }
 }
