dba2837415abda12b2e4e55afe60ad10cf7fd91e
Arka Raha <arka.raha2000@gmail.com>
2022-09-09 19:10:59 +0530

    Time: 1 ms (42.72%), Space: 42.7 MB (28.87%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,27 +1,55 @@
 class Solution {
     public void sortColors(int[] nums) {
-        if (nums.length == 0 || nums.length == 1) return;
+//         if (nums.length == 0 || nums.length == 1) return;
         
-        int start = 0;
-        int end = nums.length-1;
-        int index = 0;
+//         int start = 0;
+//         int end = nums.length-1;
+//         int index = 0;
             
-            while(index <= end && start < end){
-                if(nums[index] == 0){
-                    nums[index] = nums[start];
-                    nums[start] = 0;
-                    start ++;
-                    index++;
-                }
-                else if(nums[index] == 2){
-                    nums[index] = nums[end];
-                    nums[end] = 2;
-                    end--;
-                }
-                else{
-                    index++;
-                }
-            }
+//             while(index <= end && start < end){
+//                 if(nums[index] == 0){
+//                     nums[index] = nums[start];
+//                     nums[start] = 0;
+//                     start ++;
+//                     index++;
+//                 }
+//                 else if(nums[index] == 2){
+//                     nums[index] = nums[end];
+//                     nums[end] = 2;
+//                     end--;
+//                 }
+//                 else{
+//                     index++;
+//                 }
+//             }
         
+//     }
+// }
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0;
+        int temp;
+        while (mid <= high){
+            switch (nums[mid]){
+                case 0:{
+                    temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1:
+                    mid++;
+                    break;
+                    
+                case 2:
+                    temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+                    high --;
+                    break;
+            }
+        }
     }
 }
