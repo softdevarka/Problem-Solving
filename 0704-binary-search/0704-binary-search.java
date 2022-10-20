476bef5640dc113ba8dd3f6c69147d2e5376c00e
Arka Raha <arka.raha2000@gmail.com>
2022-10-20 22:24:48 +0530

    Time: 0 ms (100.00%), Space: 55.3 MB (5.97%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,40 @@
+//RECURSIVE SOLUTION
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int low = 0;
+//         int high = nums.length - 1;
+//         return bSearchRecursive(nums, low, high, target);
+//         }
+//     public int bSearchRecursive(int nums[], int low, int high, int target){
+//         if (low>high)
+//             return -1;
+//         int mid = (low+high)/2;
+//         if (nums[mid] == target)
+//             return mid;
+//         else if (nums[mid]>target)
+//             return bSearchRecursive(nums, low, mid-1, target);
+//         else
+//             return bSearchRecursive(nums, mid+1, high, target);
+//     }
+// }
+
+//ITERATIVE SOLUTION
+
+class Solution {
+    public int search (int nums[], int target){
+        int n = nums.length;
+        int low = 0, high = n-1;
+        while (low<=high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+            
+        }
+        return -1;
+    }
+}
