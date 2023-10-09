0bec5232d88f24e5bf0ade6837213d433cd42b85
Arka Raha <arka.raha2000@gmail.com>
2023-10-09 21:00:29 +0530

    Time: 4 ms (100%), Space: 42.5 MB (86.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev = 0;
+        int temp = x;
+        while(temp != 0){
+            int lastDigits = temp % 10;
+            rev = rev * 10 + lastDigits;
+            temp = temp/10;
+        }
+        if(rev == x) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
