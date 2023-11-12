13d89885eee36ec1649cc9fdf46633f3d8a7d3fd
Arka Raha <arka.raha2000@gmail.com>
2023-11-12 18:26:24 +0530

    Time: 2 ms (83.21%), Space: 40.7 MB (15.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,22 @@
 class Solution {
-public boolean isValid(String s) {
-    Stack<Character> st=new Stack<>();
-    for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{')
-            st.push(s.charAt(i));
-        else if(!st.isEmpty()){
-            char x=st.pop();
-            if(x=='(' && s.charAt(i)!=')')
-                return false;
-             if(x=='[' && s.charAt(i)!=']')
-                return false;
-             if(x=='{' && s.charAt(i)!='}')
-                return false;
-        }else{
-            return false;
-        }    
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (
+                stack.isEmpty() &&
+                (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']')
+            ) return false; else {
+                    if (
+                        s.charAt(i) == ')' && stack.peek() == '('
+                    ) stack.pop(); else if (
+                        s.charAt(i) == '}' && stack.peek() == '{'
+                    ) stack.pop(); else if (
+                        s.charAt(i) == ']' && stack.peek() == '['
+                    ) stack.pop(); else stack.add(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
     }
-    return (st.isEmpty())?(true):(false);
-  }
 }
