93e9001010f55229f538b1fc8c3cda36679f24f2
Arka Raha <arka.raha2000@gmail.com>
2024-03-19 22:04:41 +0530

    Time: 5 ms (52.91%), Space: 42.5 MB (96.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,15 @@
 class Solution {
-    static final int CHAR = 256;
     public boolean isAnagram(String s, String t) {
             int n = s.length();
             if(s.length() != t.length()){
             return false;}
-            int[] count = new int [CHAR];
+            int[] count = new int [26];
             for(int i=0; i<n; i++){
-                count[s.charAt(i)]++;
-                count[t.charAt(i)]--;
+                count[s.charAt(i)-'a']++;
+                count[t.charAt(i)-'a']--;
             }
 
-            for(int i=0; i<CHAR; i++){
+            for(int i=0; i<count.length; i++){
                 if(count[i] != 0) return false;
             }
             return true;
