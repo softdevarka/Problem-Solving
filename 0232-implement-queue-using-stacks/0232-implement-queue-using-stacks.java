600c6ee71306a8894a6963a0164466e27654dddf
Arka Raha <arka.raha2000@gmail.com>
2024-03-25 00:15:24 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (39.21%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,35 @@
+class MyQueue {
+
+    Stack<Integer> st1 = new Stack<>();
+    Stack<Integer> st2 = new Stack<>();
+    public MyQueue() {
+        
+    }
+    public void push(int x) {
+        st1.push(x);
+        
+    }
+    public int pop() {
+        if (st2.isEmpty()) {
+            while (!st1.isEmpty()) {
+                st2.push(st1.pop());
+            }
+        }
+        return st2.pop();
+        
+    }
+    public int peek() {
+       if(st2.isEmpty()){
+            while (!st1.isEmpty()) {
+                st2.push(st1.pop());
+            }
+       }
+        return st2.peek();
+       
+        
+    }
+    
+    public boolean empty() {
+        return st2.isEmpty() && st1.isEmpty();
+    }
+}
